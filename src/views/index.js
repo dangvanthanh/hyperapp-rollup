b6e94f07d709a7300fd560bf8f322564b219b752
Dang Van Thanh <chieuhue@gmail.com>
2017-10-21 21:30:06 +0700

    Add editorconfig, gitignore and modify hello world app

--- src/views/index.js
+++ src/views/index.js
@@ -1,13 +1,7 @@
 import { h } from 'hyperapp'
-import Slider from './slider'
 
 export default (state, actions) => (
   <div>
-    <p>Use the sliders to change background color.</p>
-    { 
-      Object.keys(state).map(color => (
-        <Slider color={color} value={state[color]} update={actions.changeBgColor} />
-      ))
-    }
+    Hello Hyperapp!
   </div>
 )
